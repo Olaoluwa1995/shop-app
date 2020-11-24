fc969bdf269f557c013f24e936509495442c5e7d
jboy457 <adejareemma@gmail.com>
2020-11-24 01:23:12 +0100

    chore: make cart dismissable

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -1,31 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
   final String imageUrl;
 
-  CartItem({this.id, this.price, this.quantity, this.title, this.imageUrl});
+  CartItem({
+    this.id,
+    this.productId,
+    this.price,
+    this.quantity,
+    this.title,
+    this.imageUrl,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: 15,
-        vertical: 4,
+    final cart = Provider.of<Cart>(context, listen: false);
+    return Dismissible(
+      key: ValueKey(id),
+      background: Container(
+        child: Icon(
+          Icons.delete,
+          color: Colors.white,
+          size: 40,
+        ),
+        alignment: Alignment.centerRight,
+        padding: const EdgeInsets.only(right: 20),
+        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5),
+          color: Theme.of(context).errorColor,
+        ),
       ),
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            child: FittedBox(child: Image.network(imageUrl)),
-            radius: 30,
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction) {
+        cart.removeItem(productId);
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 4,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              child: FittedBox(child: Image.network(imageUrl)),
+              radius: 30,
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${price * quantity}'),
+            trailing: Text('$quantity x'),
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${price * quantity}'),
-          trailing: Text('$quantity x'),
         ),
       ),
     );
