c1b0f28c6b64ab3f14fa81c09e296111cbd4a192
jboy457 <adejareemma@gmail.com>
2020-12-01 11:49:25 +0100

    fix: using future builder

--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -13,54 +13,55 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  var _isLoading = false;
-  @override
-  void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).getOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
+    final getOrder = Provider.of<Orders>(context, listen: false).getOrders();
     final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading
-          ? Center(child: CircularProgressIndicator())
-          : orderData.orders.length > 0
-              ? ListView.builder(
-                  itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-                  itemCount: orderData.orders.length,
-                )
-              : Column(children: [
-                  Center(
-                    child: Container(
-                      width: double.infinity,
-                      height: 250,
-                      margin: const EdgeInsets.only(top: 60),
-                      child: Image.network(
-                          'https://cdn.dribbble.com/users/1168645/screenshots/3152485/no-orders_2x.png'),
-                    ),
-                  ),
-                  Text(
-                    'No Orders!!',
-                    style: TextStyle(
-                        fontSize: 30,
-                        color: Theme.of(context).primaryColor,
-                        fontWeight: FontWeight.bold),
-                  )
-                ]),
+      body: FutureBuilder(
+        future: getOrder,
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          } else {
+            if (snapshot.error != null) {
+              return Center(
+                child: Text('An error occured'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => orderData.orders.length > 0
+                    ? ListView.builder(
+                        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                        itemCount: orderData.orders.length,
+                      )
+                    : Column(children: [
+                        Center(
+                          child: Container(
+                            width: double.infinity,
+                            height: 250,
+                            margin: const EdgeInsets.only(top: 60),
+                            child: Image.network(
+                                'https://cdn.dribbble.com/users/1168645/screenshots/3152485/no-orders_2x.png'),
+                          ),
+                        ),
+                        Text(
+                          'No Orders!!',
+                          style: TextStyle(
+                              fontSize: 30,
+                              color: Theme.of(context).primaryColor,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ]),
+              );
+            }
+          }
+        },
+      ),
     );
   }
 }
